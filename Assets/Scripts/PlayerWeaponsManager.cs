b79ebd2f871c7ba003c0a1655feb002bcfa3e98f
hanyemi09 <54048632+hanyemi09@users.noreply.github.com>
2021-03-23 17:03:34 +0800

    Added switching of weapons

--- Assets/Scripts/PlayerWeaponsManager.cs
+++ Assets/Scripts/PlayerWeaponsManager.cs
@@ -12,12 +12,31 @@ public class PlayerWeaponManager : MonoBehaviour
 
     WeaponController[] weaponSlots = new WeaponController[9]; // 9 available weapon slots
 
-    void AddWeapon()
+    void SwitchWeapon()
     {
-        for(int i = 0;i < weaponSlots.Length; i++)
-        {
+        // Switch to the next availiable weapon
 
+    }
+    public bool AddWeapon(WeaponController weaponPrefab)
+    {
+        for (int i = 0; i < weaponSlots.Length; i++)
+        {
+            if (weaponSlots[i] == null)
+            {
+                // spawn the weapon prefab as child of the weapon socket
+                WeaponController weaponInstance = Instantiate(weaponPrefab, weaponParentSocket);
+                weaponInstance.transform.localPosition = Vector3.zero;
+                weaponInstance.transform.localRotation = Quaternion.identity;
+
+                return true;
+            }
         }
+
+        if(GetActiveWeapon() == null)
+        {
+            SwitchWeapon();
+        }
+        return false;
     }
 
     void RemoveWeapon()
