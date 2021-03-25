63b1214d1b1ced01fdb5d1da7b0fc4357b314acb
Strafeken <strafeken@gmail.com>
2021-03-25 17:03:58 +0800

    enemies states

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float detectionRange = 10f;
+
+    protected virtual void Start()
+    {
+
+    }
+
+    protected virtual void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Enemies/EnemyManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    [SerializeField] private List<Enemy> goblins = new List<Enemy>();
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
