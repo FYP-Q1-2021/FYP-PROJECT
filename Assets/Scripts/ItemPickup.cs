936c5afee56fb6aa9d41782626891c91934c1573
hanyemi09 <54048632+hanyemi09@users.noreply.github.com>
2021-03-31 14:41:47 +0800

    Added item rotation and bobbing

---
+++ Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+
+    public Transform item;
+    [Header("Item Bob")]
+    [Range(0f, 1f)]
+    public float itemBobAmount = 0.05f;
+    [Range(0f, 10f)]
+    public float itemBobFrequency = 10f;
+
+    [Header("Item Rotation")]
+    [Range(0f, 15f)]
+    [Tooltip("Determines the speed of the item rotation on the Y Axis")]
+    public float xRotationIntensity = 10f;
+    [Range(0f, 15f)]
+    public float yRotationIntensity = 10f;
+    [Range(0f, 10f)]
+    public float rotationSmoothness = 10f;
+
+    Quaternion targetAdjustmentX;
+    Quaternion targetAdjustmentY;
+    [Tooltip("Enables rotation of item on the Y axis")]
+    [SerializeField]
+    bool yRotation;
+    [Tooltip("Enables rotation of item on the X axis")]
+    [SerializeField]
+    bool xRotation;
+
+
+    Quaternion originRotation;
+
+    Vector3 localMainPosition;
+    Vector3 localBobPosition;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        localMainPosition = transform.localPosition;
+        originRotation = transform.localRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateItemAnimation();   
+    }
+
+    void UpdateItemAnimation()
+    {
+        targetAdjustmentX = Quaternion.identity;
+        targetAdjustmentY = Quaternion.identity;
+        // item bob
+        float vBobValue = Mathf.Sin(Time.time * itemBobFrequency * 2f) * itemBobAmount;
+
+        localBobPosition.y = Mathf.Abs(vBobValue);
+        transform.localPosition = localMainPosition + localBobPosition;
+
+        // item rotation
+        if(xRotation)
+        {
+            targetAdjustmentX = Quaternion.AngleAxis(xRotationIntensity * Time.time, Vector3.right);
+        }
+        if(yRotation)
+        {
+            targetAdjustmentY = Quaternion.AngleAxis(yRotationIntensity * Time.time, Vector3.up);
+        }
+       
+        Quaternion targetRotation = originRotation * targetAdjustmentX * targetAdjustmentY;
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, targetRotation, Time.deltaTime * rotationSmoothness);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+    }
+    // on trigger enter pickup the item
+    // press button to pickup item
+    // item bob up and down with sine function
+
+}
