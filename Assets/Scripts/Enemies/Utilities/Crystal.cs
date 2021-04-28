dee0d639d61be74e23f8f7b303b0df2f104d0e45
Strafeken <strafeken@gmail.com>
2021-04-28 11:17:08 +0800

    feat: crystal explosion

--- Assets/Scripts/Enemies/Utilities/Crystal.cs
+++ Assets/Scripts/Enemies/Utilities/Crystal.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Crystal : MonoBehaviour
@@ -16,12 +15,19 @@ public class Crystal : MonoBehaviour
     [SerializeField] private float explosionRadius = 30f;
     [SerializeField] private float timeUntilExplosion = 10f;
     private float explosionCounter = 0f;
+    private LayerMask explosionLayer;
+    private bool isExplodeCoroutineRunning;
+
+    [Header("Debug Display")]
+    public Color explosionRangeColor = Color.red;
 
     private Health playerHP;
 
+    #region Unity Callbacks
     void Start()
     {
         playerHP = GameObject.FindGameObjectWithTag("Player").GetComponent<Health>();
+        explosionLayer = LayerMask.GetMask("Player");
     }
 
     void Update()
@@ -40,7 +46,8 @@ public class Crystal : MonoBehaviour
             }
             else
             {
-                StartCoroutine("Explode");
+                if(!isExplodeCoroutineRunning)
+                    StartCoroutine("Explode");
             }
         }
     }
@@ -48,41 +55,61 @@ public class Crystal : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         if (other.name == "Player")
+        {
             playerHP.Damage(projectileDamage);
+            ResetValues();
+            gameObject.SetActive(false);
+        }
     }
 
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = explosionRangeColor;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
+    }
+    #endregion
+
+    #region Public functions
     public void SetDirection(Vector3 dir)
     {
         attackDir = dir;
         // Rotate crystal
     }
+    #endregion
 
-    private void Move()
-    {
-        transform.position += attackDir * movementSpeed * Time.deltaTime;
-    }
-
+    #region Coroutines
     IEnumerator Explode()
     {
-        Collider[] nearbyObjects = Physics.OverlapSphere(transform.position, explosionRadius);
+        isExplodeCoroutineRunning = true;
+
+        Collider[] nearbyObjects = Physics.OverlapSphere(transform.position, explosionRadius, explosionLayer);
 
         for (int i = 0; i < nearbyObjects.Length; ++i)
         {
             if(nearbyObjects[i].name == "Player")
             {
-
+                playerHP.Damage(explosionDamage);
             }
         }
 
         // Particle effect
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.1f);
         ResetValues();
         gameObject.SetActive(false);
+    }
+    #endregion
+
+    #region Public functions
+    private void Move()
+    {
+        transform.position += attackDir * movementSpeed * Time.deltaTime;
     }
 
     private void ResetValues()
     {
         travelingTimeCounter = 0f;
         explosionCounter = 0f;
+        isExplodeCoroutineRunning = false;
     }
+    #endregion
 }
