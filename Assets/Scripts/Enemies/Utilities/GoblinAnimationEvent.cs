4341702adb87946fdc2acaa800b2c4c4d63eb8b8
Strafeken <strafeken@gmail.com>
2021-05-06 14:10:41 +0800

    goblin attack animation

---
+++ Assets/Scripts/Enemies/Utilities/GoblinAnimationEvent.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class GoblinAnimationEvent : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    public void StopAttackAnimation()
+    {
+        animator.SetInteger("State", (int)State.PATROL);
+    }
+}
