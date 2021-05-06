3b5956e8c2469c70db75e62bf082e2e14dc8a9bf
Strafeken <strafeken@gmail.com>
2021-05-06 22:11:27 +0800

    feat: chest interaction

--- Assets/Scripts/Utilities/ChestSpawnManager.cs
+++ Assets/Scripts/Utilities/ChestSpawnManager.cs
@@ -2,10 +2,37 @@
 
 public class ChestSpawnManager : MonoBehaviour
 {
+    [SerializeField] private float radius = 5f;
+    [SerializeField] private GameObject item;
+    private Transform player;
+    private InputHandler inputHandler;
+
     void Start()
     {
         EnemyManager.Instance.OnEnemyDeath += OnLastEnemyDeathEvent;
+        GameObject p = GameObject.FindGameObjectWithTag("Player");
+        player = p.GetComponent<Transform>();
+        inputHandler = p.GetComponent<InputHandler>();
         gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        float distance = Vector3.Distance(player.position, transform.position);
+        if(distance < radius)
+        {
+            if(inputHandler.GetInteractKeyDown())
+            {
+                Instantiate(item, transform.position, Quaternion.identity);
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
     }
 
     void OnDestroy()
