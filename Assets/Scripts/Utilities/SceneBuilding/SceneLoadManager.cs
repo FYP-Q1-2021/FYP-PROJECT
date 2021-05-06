a19e875ac2c41734e74c21132be3491495a748d2
Strafeken <strafeken@gmail.com>
2021-05-06 10:26:35 +0800

    feat: scene switching between playtest and playtest 2

---
+++ Assets/Scripts/Utilities/SceneBuilding/SceneLoadManager.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Checks if scene has been loaded
+/// </summary>
+public class SceneLoadManager : MonoBehaviour
+{
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName(scene.name));
+    }
+
+    void Start()
+    {
+        Transform spawnLocation = null;
+
+        if (!PlayerSpawnManager.Instance.isReturning)
+        {
+            if (SceneManager.GetActiveScene().name == "Playtest" || SceneManager.GetActiveScene().name == "Playtest2")
+            {
+                spawnLocation = GameObject.Find("PlayerSpawnPoint").GetComponent<Transform>();
+            }
+            else if (SceneManager.GetActiveScene().name == "Playtest3")
+            {
+                spawnLocation = GameObject.Find("PlayerSpawnPoint").GetComponent<Transform>();
+            }
+            else
+            {
+                Debug.Log("Incorrect scene name");
+            }
+        }
+        else
+        {
+            PlayerSpawnManager.Instance.isReturning = false;
+
+            if (SceneManager.GetActiveScene().name == "Playtest" || SceneManager.GetActiveScene().name == "Playtest2")
+            {
+                spawnLocation = GameObject.Find("PlayerReturnPoint").GetComponent<Transform>();
+            }
+            else if (SceneManager.GetActiveScene().name == "Playtest3")
+            {
+                spawnLocation = GameObject.Find("PlayerReturnPoint").GetComponent<Transform>();
+            }
+            else
+            {
+                Debug.Log("Incorrect scene name");
+            }
+        }
+
+        PlayerSpawnManager.Instance.player.GetComponent<CharacterController>().enabled = false;
+        PlayerSpawnManager.Instance.player.gameObject.transform.position = spawnLocation.position;
+        PlayerSpawnManager.Instance.player.gameObject.transform.rotation = Quaternion.Euler(spawnLocation.rotation.eulerAngles);
+        PlayerSpawnManager.Instance.player.GetComponent<CharacterController>().enabled = true;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+}
