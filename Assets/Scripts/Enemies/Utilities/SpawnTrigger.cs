56eae8595b2143612fc40d1e979fada004dfc814
Strafeken <strafeken@gmail.com>
2021-05-11 16:55:34 +0800

    made spawntrigger.cs subscribe to the boss death event

--- Assets/Scripts/Enemies/Utilities/SpawnTrigger.cs
+++ Assets/Scripts/Enemies/Utilities/SpawnTrigger.cs
@@ -4,9 +4,31 @@ public class SpawnTrigger : MonoBehaviour
 {
     [SerializeField] private GameObject objectToSpawn;
 
+    private Health health;
+    private bool isPassed;
+
+    void Start()
+    {
+        health = GameObject.Find("Devil").GetComponent<Health>();
+        health.OnDamaged += OnBossDeathEvent;
+    }
+
+    void OnDestroy()
+    {
+        health.OnDamaged -= OnBossDeathEvent;
+    }
+
     void OnTriggerEnter(Collider other)
     {
-        objectToSpawn.SetActive(true);
-        Destroy(gameObject);
+        if (!isPassed)
+        {
+            isPassed = true;
+            objectToSpawn.SetActive(true);
+        }
+    }
+
+    private void OnBossDeathEvent()
+    {
+        DestroyedObjectManager.Instance.AddToDestroyedObjectsList(gameObject);
     }
 }
