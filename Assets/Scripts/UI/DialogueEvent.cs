17dbaf675278c137699330c37bc262f07a656df1
Strafeken <strafeken@gmail.com>
2021-05-20 22:01:25 +0800

    feat: dialogue manager

---
+++ Assets/Scripts/UI/DialogueEvent.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+public enum DialogueTrigger
+{
+    SPAWN,
+    FIRST_GOBLIN_SIGHTED,
+    FIRST_GOBLIN_SLAYED,
+    COLLISION_WITH_BARRIER,
+    CHEST_OPENED,
+    BOSS_ROOM,
+    BOSS_SPAWNED,
+    COLLISION_WITH_DOOR,
+    BOSS_DIED
+}
+
+public class DialogueEvent : MonoBehaviour
+{
+    public DialogueTrigger trigger;
+
+    [SerializeField] private SceneLoadManager sceneLoadManager;
+    [SerializeField] private Goblin goblin;
+    [SerializeField] private OnTriggerEvent goblinViewingPoint;
+    [SerializeField] private SphereCollider barrier;
+    [SerializeField] private ChestSpawnManager chest;
+
+    void Start()
+    {
+        if (sceneLoadManager)
+            sceneLoadManager.OnSceneFinishedLoading += TriggerDialogue;
+        else if (goblin)
+        {
+            goblin.OnDeath += TriggerDialogue;
+
+            if (goblinViewingPoint)
+                goblinViewingPoint.OnTrigger += TriggerDialogue;
+        }
+        else if (barrier)
+        {
+            // Subscribe to barrier
+        }
+        else if (chest)
+            chest.OnInteract += TriggerDialogue;
+    }
+
+    void OnDestroy()
+    {
+        if (sceneLoadManager)
+            sceneLoadManager.OnSceneFinishedLoading -= TriggerDialogue;
+        else if (goblin)
+        {
+            goblin.OnDeath -= TriggerDialogue;
+            if (goblinViewingPoint)
+                goblinViewingPoint.OnTrigger -= TriggerDialogue;
+        }
+        else if (barrier)
+        {
+            // Unsubscribe to barrier
+        }
+        else if (chest)
+            chest.OnInteract -= TriggerDialogue;
+    }
+
+    public void TriggerDialogue()
+    {
+        DialogueManager.Instance.ShowDialogue(trigger);
+    }
+}
---
+++ Assets/Scripts/UI/DialogueManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    public static DialogueManager Instance { get; private set; }
+
+    public List<DialogueData> dialogueDatas = new List<DialogueData>();
+
+    [Header("Variables")]
+    [SerializeField] private GameObject dialogueBox;
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private float duration = 5f;
+    bool isCoroutineRunning;
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        dialogueBox.SetActive(false);
+    }
+
+    public void ShowDialogue(DialogueTrigger trigger)
+    {
+        for(int i = 0; i < dialogueDatas.Count; ++i)
+        {
+            if(dialogueDatas[i].trigger == trigger)
+            {
+                text.text = dialogueDatas[i].dialogue;
+                if (isCoroutineRunning)
+                    StopCoroutine("Dialogue");
+                StartCoroutine("Dialogue");
+                return;
+            }
+        }
+    }
+
+    IEnumerator Dialogue()
+    {
+        isCoroutineRunning = true;
+
+        dialogueBox.SetActive(true);
+        yield return new WaitForSeconds(duration);
+        dialogueBox.SetActive(false);
+
+        isCoroutineRunning = false;
+    }
+}
