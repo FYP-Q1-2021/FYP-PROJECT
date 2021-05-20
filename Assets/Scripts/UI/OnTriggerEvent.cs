93609145bfe8f2c444a895510d33fe5974bfb3a0
Strafeken <strafeken@gmail.com>
2021-05-20 22:03:18 +0800

    feat: viewpoint trigger event for first goblin sighting

---
+++ Assets/Scripts/UI/OnTriggerEvent.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System;
+
+public class OnTriggerEvent : MonoBehaviour
+{
+    public event Action OnTrigger;
+
+    bool triggered;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(!triggered && other.name == "Player")
+        {
+            OnTrigger?.Invoke();
+            triggered = true;
+        }
+    }
+}
